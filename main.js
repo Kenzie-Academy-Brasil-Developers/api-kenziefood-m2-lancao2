cf8457e0f42058455f88208c95bc238b8b566c6e
igorscotton <scotton.igor@gmail.com>
2022-03-22 16:17:37 -0300

    feat/ função de filtrar produtos pelo input de pesquisa criada

--- main.js
+++ main.js
@@ -3,5 +3,12 @@ import { KenzieFood } from "./src/utils/KenzieFood.js";
 (async function main () {
   const products = await KenzieFood.getProducts()
   
+  
+  
+  const input = document.querySelector('.title--search')
+  input.addEventListener('keyup', (event) => {
+    KenzieFood.filterProducts(event, products)
+  })
+  
   console.log(products)
 })()
