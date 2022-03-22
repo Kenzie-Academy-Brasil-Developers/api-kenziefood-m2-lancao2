83b98f978b1353e4b2ae108314a28b637ab22581
igorscotton <scotton.igor@gmail.com>
2022-03-22 16:48:38 -0300

    alteração do nome da  classe de cada card do carrinho

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { KenzieFood } from "./src/utils/KenzieFood.js";
 
 (async function main () {
-  const products = await KenzieFood.getProducts()
-  
+  const products = await KenzieFood.getProducts()  
+    
   console.log(products)
 })()
