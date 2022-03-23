cb13ef65dbe30ad4d2ebe9893ad32d662cc057d3
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-03-23 08:46:11 -0300

    feature/ :sparkles: Adicionada função para fechar o modal

--- main.js
+++ main.js
@@ -2,28 +2,37 @@ const section = document.querySelector("section")
 import { ShowCase } from "./src/models/Showcase.js";
 import { KenzieFood } from "./src/utils/KenzieFood.js";
 
-
+/* ============== Main function ==================*/
 (async () => {
   const products = await KenzieFood.getProducts()
   ShowCase.showProducts(products)
 })()
+/* ============== End main function ==============*/
 
+const openModal = () => {
+  const modal = document.getElementById("modal")
+  modal.style.display = "block"
+}
+
+const closeModal = e => {
+  const clickedElement = e.target
+  const classesThatClose = ['modal-wrapper', 'close-modal']
+
+  classesThatClose.forEach(clas => {
+    const isAClosingClass = [...clickedElement.classList].includes(clas)
+
+    if (isAClosingClass) {
+      const modal = document.getElementById("modal")
+      modal.style.display = "none"
+    }
+  })
+}
   
 const input = document.querySelector('.title--search')
 input.addEventListener('keyup', KenzieFood.filterProducts)
 
-// função de criar e fechar modal na versao mobile
-
 const buttonOpenCard = document.getElementById("modalButton")
-buttonOpenCard.addEventListener("click", () =>{
-  const modal = document.getElementById("modal")
-  modal.style.display = "block"
-} )
+buttonOpenCard.addEventListener("click", openModal)
 
-const conteinerCart = document.querySelector(".conteiner_aside")
 const aside = document.querySelector("aside")
-aside.addEventListener("click", event => {
-  // fazer função para fechar o modal
-  
-})
-// ======= fim da função criar e fechar modal na versao mobile
+aside.addEventListener("click", closeModal)
