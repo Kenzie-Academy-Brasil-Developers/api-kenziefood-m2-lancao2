2a303ed5b2ad68f677ca3bc95eee3f1fa82cc911
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-03-22 21:30:00 -0300

    deat/ :sparkles: Adicionado filtro por nome através do input

--- src/models/Showcase.js
+++ src/models/Showcase.js
@@ -1,15 +1,14 @@
-import { KenzieFood } from "../utils/KenzieFood.js"
 import { createCard } from "../utils/functions.js"
 
 export class ShowCase {
     static container = document.querySelector('.products-container')
     static products = []
-   
-    static async showProducts () {
-      const products = await KenzieFood.getProducts()
-      ShowCase.products = products
 
-      this.products.forEach(product => {
+    static async showProducts (products) {
+      ShowCase.products = products
+      ShowCase.container.innerHTML = ''
+
+      products.forEach(product => {
         ShowCase.container.innerHTML += createCard(product)
       })
     }
