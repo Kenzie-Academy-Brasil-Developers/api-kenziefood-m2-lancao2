b1afd337a705f2873c7ea91d3f48002dc8ec9ac9
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-03-24 10:33:41 -0300

    :sparkles: Implementada função de registro de usuários

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,9 +1,12 @@
+import { KenzieFood } from "../../src/utils/KenzieFood.js"
+
 const getNewUserData = e => {
   e.preventDefault()
 
   const form = new FormData(e.target)
   const userData = Object.fromEntries(form.entries())
-  console.log(userData)
+  
+  KenzieFood.userRegister(userData)
 }
 
 const form = document.querySelector('.form-container')
